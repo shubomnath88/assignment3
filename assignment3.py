8bb8dc9e4e8ec24703961b4c2cca58eee4c4dff5
shubomnath88 <shubomnath88@gmail.com>
2018-06-14 17:48:59 +0530

    assignment3 done

---
+++ assignment3.py
@@ -0,0 +1,33 @@
+#ques1
+a = input("enter anything")
+l=[]
+print(l.append(a))
+print(l)
+#ques2
+l = ['google','apple','microsoft','facebook','tesla']
+print(l)
+#ques3
+a = [1,8,4,5,2,1,4,3,5,4,8,2]
+print(a.count(4))
+print(a)
+#ques4
+a = [2,5,3,8,4,1,0,6]
+print(a.sort())
+print(a)
+#ques5
+a = [5,3,8]
+b = [6,2,4]
+c = a+b
+print(c)
+print(c.sort())
+print(c)
+#ques6
+a = []
+print(a.append('shubom'))
+print(a.append(' mishon'))
+print(a.append(' nath'))
+print(a)
+print(a.pop(1))
+print(a)
+print(a.pop())
+print(a)
