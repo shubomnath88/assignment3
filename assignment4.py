f210676c653a2607b7d5beae85697d60d49ba950
shubomnath88 <shubomnath88@gmail.com>
2018-06-15 21:34:02 +0530

    assignment4 done

---
+++ assignment4.py
@@ -0,0 +1,34 @@
+#TUPLES
+#ques1
+a = (1,5,6,3,8)
+print(len(a))
+#ques2
+print(min(a))
+print(max(a))
+#ques3
+t = (1*2*3*4*5)
+print(t)
+#SETS
+#ques1
+a = set((1,3,5,2,8,9))
+b = set((3,7,4,6,2,8))
+print(a-b)
+print(b-a)
+#ques2
+c = set((1,2,3,4))
+d = set((2,3))
+print(c>=d)
+print(c<=d)
+#ques3
+print(a&b)
+#directories
+#ques1
+d = {"name":"xyz","marks":90}
+print(d)
+#ques3
+l = ['m','i','s','s','i','s','s','i','p','p','i']
+print(l.count('m'))
+print(l.count('i'))
+print(l.count('s'))
+print(l.count('p'))
+print(l)
